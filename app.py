0d3fb560b2c38c4b13ce1f085e4db27fde626270
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-10-24 16:15:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -317,7 +317,7 @@ def extract_text_from_pdf(pdf_file):
     except Exception as e:
         return f"Error reading PDF: {str(e)}"
 
-set_page_background_and_style("Gemini_Generated_Image_en6x3oen6x3oen6x.png")
+set_page_background_and_style("Gemini_Generated_Image_nqw9yanqw9yanqw9.png")
 
 # Database setup
 DB_PATH = "homeo_clinic.json"
